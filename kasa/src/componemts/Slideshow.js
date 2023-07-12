1414566ad27f7eb54b303edcd7ba80de23b6af40
BenoitDx <benoit.berche@gmail.Com>
2023-07-12 16:06:24 +0200

    avancement logement

--- kasa/src/componemts/Slideshow.js
+++ kasa/src/componemts/Slideshow.js
@@ -6,36 +6,40 @@ import '../styles/Slideshow.css';
 const Slideshow = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  // Fonction pour afficher la prochaine image
+// Fonction pour afficher la prochaine image
   const nextSlide = () => {
     setCurrentImageIndex((prevIndex) =>
       prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
   };
 
-  // Fonction pour afficher l'image précédente
+// Fonction pour afficher l'image précédente
   const previousSlide = () => {
     setCurrentImageIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
+// Calcul du compteur de l'image actuelle sur le total des images
+  const imageCount = `${currentImageIndex + 1}/${images.length}`;
 
   return (
     <div className="slideshow">
-     
-      <img
-        className="arrow left-arrow"
-        src={leftArrow}
-        alt="Previous"
-        onClick={previousSlide}
-      />
-      <img className='img-show' src={images[currentImageIndex]} alt="Slideshow" />
-      <img
-        className="arrow right-arrow"
-        src={rightArrow}
-        alt="Next"
-        onClick={nextSlide}
-      />
+      <div className="image-container">
+        <img
+          className="arrow left-arrow"
+          src={leftArrow}
+          alt="Previous"
+          onClick={previousSlide}
+        />
+        <img className='img-show' src={images[currentImageIndex]} alt="Slideshow" />
+        <img
+          className="arrow right-arrow"
+          src={rightArrow}
+          alt="Next"
+          onClick={nextSlide}
+        />
+        <div className="image-count">{imageCount}</div>
+      </div>
     </div>
   );
 };
